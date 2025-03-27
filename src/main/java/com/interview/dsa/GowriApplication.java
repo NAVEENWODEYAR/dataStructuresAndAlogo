4c513acdd7e3e548a91c5f10af2911670c05762a
Naveen <naveenkrishnawodeyar@gmail.com>
2025-03-27 23:33:41 +0530

    initial commit

---
+++ src/main/java/com/interview/dsa/GowriApplication.java
@@ -0,0 +1,14 @@
+package com.interview.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GowriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GowriApplication.class, args);
+	}
+
+}
+dataStructuresAndAlogo
---
+++ src/test/java/com/interview/dsa/GowriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GowriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
