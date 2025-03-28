c978d915c2acc90dbe5b42b0ec76d539833b93ad
Naveen <naveenkrishnawodeyar@gmail.com>
2025-03-29 00:09:17 +0530

    properties added

--- src/main/java/com/interview/dsa/GowriApplication.java
+++ src/main/java/com/interview/dsa/GowriApplication.java
@@ -16,34 +16,31 @@ public class GowriApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(GowriApplication.class);
 
-	// Track application start time and stop time
 	private Instant startTime;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GowriApplication.class, args);
+		logger.info("Application running successfully");
 	}
 
-	// Listener for ApplicationStartedEvent (When application is starting)
 	@EventListener(ApplicationStartedEvent.class)
 	public void onApplicationStart() {
 		startTime = Instant.now();
-		logger.info("Application started at: " + startTime);
+		logger.info("Application started at:{}",startTime);
 	}
 
-	// Listener for ApplicationReadyEvent (When application is fully started and ready)
 	@EventListener(ApplicationReadyEvent.class)
 	public void onApplicationReady() {
 		logger.info("Application is running.");
 	}
 
-	// Listener for ContextClosedEvent (When application is stopping)
 	@EventListener(ContextClosedEvent.class)
 	public void onApplicationStop() {
 		Instant stopTime = Instant.now();
-		logger.info("Application stopped at: " + stopTime);
+		logger.info("Application stopped at:{}",stopTime);
 		if (startTime != null) {
 			long duration = stopTime.getEpochSecond() - startTime.getEpochSecond();
-			logger.info("Application ran for: " + duration + " seconds.");
+			logger.info("Application ran for:{}",duration+" seconds.");
 		}
 	}
 }
